c58b013cdfca54844a2a4098e189e1e676db66d0
Acquah <emanuel.molina2k@gmail.com>
2023-04-03 23:01:30 -0700

    couldnt figure out this project at all

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,72 @@
-function printScore(){
-    // getting highscores from the local storage or making an array
-    var highScore = JSON.parse(window.localStorage.getItem("highscore")) || [];
+var listQuiz = document.querySelector("ul");
+var startQuiz = document.querySelector("#startQuiz");
+var time = document.querySelector("#time");
+var quizQuestions = document.querySelector("#quizQuestions");
+var 
+var
+var
 
-    //  sorts highscores by descending order
-    highScore.sort(function(a, b) {
-        return b.score - a.score;
+var timePlaceHolder = 0;
+var secondTimer = 60;
+var pickedQuestions = 0;
+
+startQuiz.addEventListener ("click" = function(){
+    if(timePlaceHolder === 0){
+        timePlaceHolder = setInterval (function(){
+            secondTimer--;
+            time.textContent = "Remaining Time: " + secondTimer;
+
+            if (secondTimer <= 0){
+                clearInterval(timeHolder);
+                time.textContent = "TIMES UP";
+            }
+        }, 1000);
     }
+    output(pickedQuestions);
+});
+
+function output(pickedQuestions){
+    quizQuestions.innerHTML = "";
+    listQuiz.innerHTML = "";
+    for (var i = 0; i < questions.length; i++){
+        var userInput = questions[pickedQuestions].ask;
+        var userOutput = questions[pickedQuestions].choices;
+
+        quizQuestions.textContent = userInput;
+    }
+    userOutput.forEach(function (line){
+        var listCreated = document.createElement("li");
+        listCreated.textContent = line;
+        quizQuestions.appendChild(listQuiz);
+        listQuiz.appendChild(listCreated);
+        listCreated.addEventListener("click", (compare));
+    })
 }
+var questions = [
+    {
+        title: "Which tag is used to create a hyperlink?",
+        choices: ["<a>", "<img>", "<ol>", "<link>"],
+        answer: "<link>"
+    },
+    {
+        title: "The condition in an if / else statment is enclosed within _____.",
+        choices: ["parentheses", "quotes", "curly brackets", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "What does css stand for?",
+        choices: ["Cascading Style Sheets", "Corresponding Style Sheets", "Coral Starfish Sea", "customer service support"],
+        answer: "Cascading Style Sheets"
+    },
+    {
+        title: "What html tag is NOT included in the HEAD tag?",
+        choices: ["link", "meta", "title", "header"],
+        answer: "header"
+    },
+    {
+        title: "What attribute is used in html to decorate content?",
+        choices: ["css", "class", "src", "style"],
+        answer: "style"
+    },
+];
+
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# coding-quiz
+# Coding Quiz
+
+## Technology Used
+
+| Technology Used         | Resource URL  |
+| -------------           |:-------------:|
+| HTML                    | [https://developer.mozilla.org/en-US/docs/Web/HTML](https://developer.mozilla.org/en-US/docs/Web/HTML) | 
+| CSS                     | [https://developer.mozilla.org/en-US/docs/Web/CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)      |
+| Git                     | [https://git-scm.com/](https://git-scm.com/)     | 
+| JavaScript              | [https://developer.mozilla.org/en-US/docs/Web/JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript) |
+## Description
+
+## Author information
+
+```MD
+### Emanuel Molina
+
+[GitHub](https://github.com/AcquahLopid)
+[LinkedIn](https://www.linkedin.com/in/emanuel-molina-65b84426b/)
+[GitLab](https://ucb.bootcampcontent.com/emanuelmolina2k-BAsayK)
+
+```
+# Credits
